d7b1fb7ebbe52480db6a2f3053cfa84a5e3b8eba
Victor Ribero <victor.ribero3@gmail.com>
2019-10-30 20:19:03 +0100

    Created Footer config. Got Personal social media from a side-project

---
+++ src/config/footer.js
@@ -0,0 +1,16 @@
+export const links = [{
+  label: 'Github',
+  url: 'https://github.com/tureey'
+}, {
+  label: 'Linkedin',
+  url: 'https://www.linkedin.com/in/victorribero/'
+}, {
+  label: 'Twitter',
+  url: 'https://twitter.com/devictoribero'
+}, {
+  label: 'Personal site',
+  url: 'https://www.elrincondevictor.com/'
+}, {
+  label: 'Side project',
+  url: 'https://www.chooseyourplant.com/'
+}]
